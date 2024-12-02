6697ce007b234fb03298c4145ae469b2b488678e
harryd13 <harman13deep@gmail.com>
2024-12-02 13:43:58 +0530

    get

---
+++ src/main/java/com/rackserver/Klothes/KlothesApplication.java
@@ -0,0 +1,13 @@
+package com.rackserver.Klothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rackserver/Klothes/KlothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rackserver.Klothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
